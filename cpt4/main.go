b7aae8acad0dec3b59eb7c44a287170a0cf98936
xuhaoshen <haoshenxv@126.com>
2020-02-16 14:38:46 +0800

    chapter 4

--- cpt4/main.go
+++ cpt4/main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	//"fmt"
-	//"tutorial/cpt2/popcount"
-	"fmt"
-	"tutorial/cpt4/solution"
-)
-
 func main() {
 	// 4.1
 	//s1 := popcount.GetPc()
@@ -18,36 +11,41 @@ func main() {
 	//solution.ShaOutput()
 
 	// 4.3 此处指针数组的用法与C语言非常相似
-	array := [5]int{1, 2, 3, 4, 5}
-	solution.ReverseRemaster(&array)
-	fmt.Printf("4.3 Reverse: %x \n", array)
+	//array := [5]int{1, 2, 3, 4, 5}
+	//solution.ReverseRemaster(&array)
+	//fmt.Printf("4.3 Reverse: %x \n", array)
 
 	// 4.4
-	array2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-	res := solution.RotateOnce(&array2, 3)
-	fmt.Printf("4.4 Rotate: %x \n", res)
+	//array2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	//res := solution.RotateOnce(&array2, 3)
+	//fmt.Printf("4.4 Rotate: %x \n", res)
 
 	// 4.5
-	array3 := []string{"bbbb", "bbbb", "bbbb", "bbbb"}
-	res5 := solution.RmSameChar(array3)
-	fmt.Print("4.5 Result: ", res5, "\n")
+	//array3 := []string{"bbbb", "bbbb", "bbbb", "bbbb"}
+	//res5 := solution.RmSameChar(array3)
+	//fmt.Print("4.5 Result: ", res5, "\n")
 
 	// 4.6
-	array5 := []rune("1 234567")
-	res6 := solution.RmUniSame(array5)
-	fmt.Print("4.6 RmUniSame ", res6, "\n")
+	//array5 := []rune("1 234567")
+	//res6 := solution.RmUniSame(array5)
+	//fmt.Print("4.6 RmUniSame ", res6, "\n")
 
 	// 4.7
-	array4 := []byte("我要搞Golang")
-	res7 := solution.RevRemaster(array4)
-	fmt.Print("4.7 Revert Result", res7, "\n")
+	//array4 := []byte("我要搞Golang")
+	//res7 := solution.RevRemaster(array4)
+	//fmt.Print("4.7 Revert Result", res7, "\n")
 
 	// 4.8
-	fmt.Println("-------- Input 4.8 --------")
-	solution.Charcount()
+	//fmt.Println("-------- Input 4.8 --------")
+	//solution.Charcount()
 
 	// 4.9
-	fmt.Println("-------- Input 4.9 --------")
-	solution.Wordfreq()
+	//fmt.Println("-------- Input 4.9 --------")
+	//solution.Wordfreq()
+
+	// 4.10 go run main.go gopl.io/ch4/issues
+	//fmt.Println("-------- Input 4.10 -------")
+	//solution.SearchIssuesMainProcess()
+
 
 }
